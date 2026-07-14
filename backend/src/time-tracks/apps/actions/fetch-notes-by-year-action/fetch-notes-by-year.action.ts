ff354d97e4fc4ec5ef97ae79e54c0d1c115280e6
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-07-13 21:40:39 -0400

    rename(time-tracks): get-notes-by-year-action → fetch-notes-by-year-action (part 1)

--- backend/src/time-tracks/apps/actions/fetch-notes-by-year-action/fetch-notes-by-year.action.ts
+++ backend/src/time-tracks/apps/actions/fetch-notes-by-year-action/fetch-notes-by-year.action.ts
@@ -1,19 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { TimeTrackService } from '../../../domain/services/time-track-service/time-track.service';
 import {
   GetAuthUser,
   AuthUser,
 } from 'src/shared-kernel/apps/decorators/get-auth-user.decorator';
 import { ProtectedAction } from 'src/shared-kernel/apps/decorators/protected-action.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/shared-kernel/apps/guards/jwt-auth.guard';
 import { NotesByYearResponseDto } from '../../dtos/responses/notes-by-year.response.dto';
-import { GetNotesByYearSwagger } from 'src/time-tracks/apps/actions/get-notes-by-year-action/get-notes-by-year.swagger';
+import { FetchNotesByYearSwagger } from './fetch-notes-by-year.swagger';
 
 @Controller('time-tracks')
-export class GetNotesByYearAction {
+@UseGuards(JwtAuthGuard)
+@ApiTags('Time Tracks')
+@ApiBearerAuth()
+export class FetchNotesByYearAction {
   constructor(private readonly timeTrackService: TimeTrackService) {}
 
   @Get('/notes-by-year')
-  @ProtectedAction(GetNotesByYearSwagger)
+  @ProtectedAction(FetchNotesByYearSwagger)
   async apply(@GetAuthUser() user: AuthUser): Promise<NotesByYearResponseDto> {
     return await this.timeTrackService.getNotesByYear({ user });
   }
