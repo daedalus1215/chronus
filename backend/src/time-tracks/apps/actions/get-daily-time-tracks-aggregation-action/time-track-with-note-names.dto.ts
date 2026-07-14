b5a581594f6a3a351c959b154ea0d6973e24336e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-07-13 21:40:40 -0400

    rename(time-tracks): get-daily-time-tracks-aggregation-action → fetch-daily-time-tracks-aggregation-action (part 1)

--- backend/src/time-tracks/apps/actions/get-daily-time-tracks-aggregation-action/time-track-with-note-names.dto.ts
+++
@@ -1,8 +0,0 @@
-export type TimeTrackWithNoteNamesDto = {
-  noteId: number;
-  noteName: string;
-  totalTimeMinutes: number;
-  dailyTimeMinutes: number;
-  mostRecentStartTime: string;
-  mostRecentDate: string;
-};
