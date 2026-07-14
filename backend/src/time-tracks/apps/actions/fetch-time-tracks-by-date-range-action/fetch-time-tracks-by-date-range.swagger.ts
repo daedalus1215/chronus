55c50cf279a7098a75f0e2beb87d51e1ae7cde9d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-07-13 21:40:40 -0400

    rename(time-tracks): get-time-tracks-by-date-range-action → fetch-time-tracks-by-date-range-action (part 2)

---
+++ backend/src/time-tracks/apps/actions/fetch-time-tracks-by-date-range-action/fetch-time-tracks-by-date-range.swagger.ts
@@ -0,0 +1,14 @@
+import { ProtectedActionOptions } from 'src/shared-kernel/apps/decorators/protected-action.decorator';
+import { TimeTrackWithNoteResponse } from '../../dtos/responses/time-track-with-note.response.dto';
+
+export const FetchTimeTracksByDateRangeSwagger: ProtectedActionOptions = {
+  tag: 'Time Tracks',
+  summary: 'Get raw individual time tracks within a date range',
+  additionalResponses: [
+    {
+      status: 200,
+      description: 'Time tracks within the date range.',
+      type: [TimeTrackWithNoteResponse],
+    },
+  ],
+};
