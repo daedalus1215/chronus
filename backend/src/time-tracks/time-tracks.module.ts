79fe0ab046169d9213184d462ad3fc026969e583
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-07-13 21:40:39 -0400

    refactor(time-tracks): remove flat update-time-track-note files; update module imports for -TS paths

--- backend/src/time-tracks/time-tracks.module.ts
+++ backend/src/time-tracks/time-tracks.module.ts
@@ -2,32 +2,32 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TimeTrack } from './domain/entities/time-track-entity/time-track.entity';
 import { TimeTrackRepository } from './infra/repositories/time-track.repository';
-import { CreateTimeTrackTransactionScript } from './domain/transaction-scripts/create-time-track.transaction.script';
+import { CreateTimeTrackTransactionScript } from './domain/transaction-scripts/create-time-track-TS/create-time-track.transaction.script';
 import { GetNoteTimeTracksTransactionScript } from './domain/transaction-scripts/get-note-time-tracks-TS/get-note-time-tracks.transaction.script';
 import { CreateTimeTrackAction } from './apps/actions/create-time-track-action/create-time-track.action';
-import { GetTimeTracksByNoteIdAction } from './apps/actions/get-time-tracks-by-note-id-action/get-time-tracks-by-note-id.action';
-import { GetTimeTracksTotalByNoteIdAction } from './apps/actions/get-time-tracks-total-by-note-id-action/get-time-tracks-total-by-note-id.action';
+import { FetchTimeTracksByNoteIdAction } from './apps/actions/fetch-time-tracks-by-note-id-action/fetch-time-tracks-by-note-id.action';
+import { FetchTimeTracksTotalByNoteIdAction } from './apps/actions/fetch-time-tracks-total-by-note-id-action/fetch-time-tracks-total-by-note-id.action';
 import { GetTimeTracksTotalByNoteIdTransactionScript } from './domain/transaction-scripts/get-time-tracks-total-by-note-id-TS/get-time-tracks-total-by-note-id.transaction.script';
 import { GetDailyTimeTracksAggregationTransactionScript } from './domain/transaction-scripts/get-daily-time-tracks-aggregation-TS/get-daily-time-tracks-aggregation.transaction.script';
 import { NotesModule } from '../notes/notes.module';
 import { TagsModule } from '../tags/tags.module';
 import { TimeTrackService } from './domain/services/time-track-service/time-track.service';
-import { TimeTrackWithNoteNamesResponder } from './apps/actions/get-daily-time-tracks-aggregation-action/time-track-with-note-names.responder';
+import { TimeTrackWithNoteNamesResponder } from './apps/actions/fetch-daily-time-tracks-aggregation-action/time-track-with-note-names.responder';
 import { DeleteTimeTrackAction } from './apps/actions/delete-time-track-action/delete-time-track.action';
 import { DeleteTimeTrackTransactionScript } from './domain/transaction-scripts/delete-time-track.transaction.script';
 import { UpdateTimeTrackNoteAction } from './apps/actions/update-time-track-note-action/update-time-track-note.action';
-import { UpdateTimeTrackNoteTransactionScript } from './domain/transaction-scripts/update-time-track-note.transaction.script';
-import { UpdateTimeTrackPayloadConverter } from './domain/transaction-scripts/update-time-track-note.converter';
-import { GetDailyTimeTracksAction } from './apps/actions/get-daily-time-tracks-aggregation-action/get-daily-time-tracks-aggregation.action';
-import { GetWeeklyMostActiveNoteAction } from './apps/actions/get-weekly-most-active-note-action/get-weekly-most-active-note.action';
+import { UpdateTimeTrackNoteTransactionScript } from './domain/transaction-scripts/update-time-track-note-TS/update-time-track-note.transaction.script';
+import { UpdateTimeTrackPayloadConverter } from './domain/transaction-scripts/update-time-track-note-TS/update-time-track-note.converter';
+import { FetchDailyTimeTracksAction } from './apps/actions/fetch-daily-time-tracks-aggregation-action/fetch-daily-time-tracks-aggregation.action';
+import { FetchWeeklyMostActiveNoteAction } from './apps/actions/fetch-weekly-most-active-note-action/fetch-weekly-most-active-note.action';
 import { GetWeeklyMostActiveNoteTransactionScript } from './domain/transaction-scripts/get-weekly-most-active-note-TS/get-weekly-most-active-note.transaction.script';
-import { GetWeeklyTrendAction } from './apps/actions/get-weekly-trend-action/get-weekly-trend.action';
+import { FetchWeeklyTrendAction } from './apps/actions/fetch-weekly-trend-action/fetch-weekly-trend.action';
 import { GetWeeklyTrendTransactionScript } from './domain/transaction-scripts/get-weekly-trend-TS/get-weekly-trend.transaction.script';
 import { GetStreakTransactionScript } from './domain/transaction-scripts/get-streak-TS/get-streak.transaction.script';
-import { GetStreakAction } from './apps/actions/get-streak-action/get-streak.action';
-import { GetNotesByYearAction } from './apps/actions/get-notes-by-year-action/get-notes-by-year.action';
+import { FetchStreakAction } from './apps/actions/fetch-streak-action/fetch-streak.action';
+import { FetchNotesByYearAction } from './apps/actions/fetch-notes-by-year-action/fetch-notes-by-year.action';
 import { GetNotesByYearTransactionScript } from './domain/transaction-scripts/get-notes-by-year-TS/get-notes-by-year.transaction.script';
-import { GetTimeTracksByDateRangeAction } from './apps/actions/get-time-tracks-by-date-range-action/get-time-tracks-by-date-range.action';
+import { FetchTimeTracksByDateRangeAction } from './apps/actions/fetch-time-tracks-by-date-range-action/fetch-time-tracks-by-date-range.action';
 import { GetTimeTracksByDateRangeTransactionScript } from './domain/transaction-scripts/get-time-tracks-by-date-range-TS/get-time-tracks-by-date-range.transaction.script';
 import { TimeTracksAggregator } from './domain/aggregators/time-tracks.aggregator';
 import { TIME_TRACK_WRITER_PORT } from '../note-transfer/domain/ports/time-track-writer.port';
@@ -58,16 +58,16 @@ import { TIME_TRACK_WRITER_PORT } from '../note-transfer/domain/ports/time-track
   ],
   controllers: [
     CreateTimeTrackAction,
-    GetTimeTracksByNoteIdAction,
-    GetTimeTracksTotalByNoteIdAction,
-    GetDailyTimeTracksAction,
+    FetchTimeTracksByNoteIdAction,
+    FetchTimeTracksTotalByNoteIdAction,
+    FetchDailyTimeTracksAction,
     DeleteTimeTrackAction,
     UpdateTimeTrackNoteAction,
-    GetWeeklyMostActiveNoteAction,
-    GetWeeklyTrendAction,
-    GetStreakAction,
-    GetNotesByYearAction,
-    GetTimeTracksByDateRangeAction,
+    FetchWeeklyMostActiveNoteAction,
+    FetchWeeklyTrendAction,
+    FetchStreakAction,
+    FetchNotesByYearAction,
+    FetchTimeTracksByDateRangeAction,
   ],
   exports: [TimeTracksAggregator, TIME_TRACK_WRITER_PORT],
 })
